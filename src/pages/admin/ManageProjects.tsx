dbf914f6fb5236390ab3b200b32fcee21854a168
mithunpagadwar8 <mithunpagadwar8@gmail.com>
2025-12-11 00:07:08 +0530

    Update ManageProjects.tsx

--- src/pages/admin/ManageProjects.tsx
+++ src/pages/admin/ManageProjects.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { getStoredData, saveStoredData, fileToBase64 } from '../../services/storage';
+import { getStoredData, setStoredData, fileToBase64 } from '../../services/storage';
 import { AppData, Project } from '../../types';
 import { Plus, Trash2, Upload, FileSpreadsheet } from 'lucide-react';
 
