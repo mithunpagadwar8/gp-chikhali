964212ab3be9a009006e648db5b7a117a4ff8c04
mithunpagadwar8 <mithunpagadwar8@gmail.com>
2025-12-11 03:50:57 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import * as React from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
+import { AuthProvider } from "./providers/AuthProvider";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("Root element not found");
+}
+
+createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
